852009fa5fbce0b72d477983bda6ed2454eea516
Taha Enayat <ta.enayat@gmail.com>
2024-11-29 21:25:40 +0100

    slurm file for hyper parameter optimization

---
+++ bash_scripts/hyperparam_opt.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Job name
+#SBATCH --job-name cyclegan
+#SBATCH --output /mnt/homeGPU/tenayat/slurm_archive/slurm-%j.out
+#SBATCH --partition dgx
+
+#SBACTH --mem 64G
+
+# Use GPU
+#SBATCH --gres=gpu:1
+
+# Default configs for NGPU
+export PATH="/opt/anaconda/anaconda3/bin:$PATH"
+export PATH="/opt/anaconda/bin:$PATH"
+eval "$(conda shell.bash hook)"
+export TFHUB_CACHE_DIR=.
+
+set -euo pipefail
+
+# Activating conda enviroment
+# conda activate /mnt/homeGPU/tenayat/conda4ganslate
+conda activate /mnt/homeGPU/tenayat/cuda11
+
+cd /mnt/homeGPU/tenayat
+
+METRIC="mae_clean_mask"
+
+python python_scripts/optimizer.py
+
+printf "\n\nFINISHED\n\n"
