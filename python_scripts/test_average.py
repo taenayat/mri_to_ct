af6dd1ca9505003482e2ce29a75a5f0a413c6a38
Taha Enayat <ta.enayat@gmail.com>
2024-12-05 19:59:22 +0100

    added confidence interval instead of standard deviation

--- python_scripts/test_average.py
+++ python_scripts/test_average.py
@@ -1,7 +1,10 @@
 """
-Add a line to the csv file containing the metrics for test data. currently the test
-metrics CSV file adds a line for metrics of each image in the test folder. 
-This script adds one line at the end with the average value of each column.
+Add a line to the csv file containing the metrics for test data of all experiments. currently the test
+metrics CSV file adds a line for metrics of each image in two of the global CSV files. 
+In `meta_test_stats`, it adds one line and two column for each statistics; one for the average
+value and one for the confidence interval (standard deviation)
+In `meta_test_stats_hr` which is the human readable version of the file, it combines the average
+and confidence interval.
 """
 
 import pandas as pd
@@ -39,9 +42,9 @@ def extract_name(filepath):
             return parts[-3] + "_" + parts[-1].replace('.yaml', '')
     return "Invalid Path"
 
-print(YAML_PATH)
-print(generate_metrics_path(YAML_PATH))
-print(extract_name(YAML_PATH))
+# print(YAML_PATH)
+# print(generate_metrics_path(YAML_PATH))
+# print(extract_name(YAML_PATH))
 
 metrics_df = pd.read_csv(generate_metrics_path(YAML_PATH))
 
@@ -79,12 +82,19 @@ name = extract_name(YAML_PATH)
 avg_df = metrics_df.mean(axis=0).to_frame().T.drop(['Unnamed: 0'], axis=1)
 std_df = metrics_df.std(axis=0).to_frame().T.drop(['Unnamed: 0'], axis=1)
 
-# formatted_df = pd.DataFrame({col: f"{avg:.2f} ± {std:.2f}" for col, avg, std in zip(avg_df.columns, avg_df.iloc[0], std_df.iloc[0])})
-formatted_df = pd.Series({col: f"{avg:.2f} ± {std:.2f}" for col, avg, std in zip(avg_df.columns, avg_df.iloc[0], std_df.iloc[0])}).to_frame().T
+n = len(metrics_df)
+critical_value = 2.042
+confidence_interval = critical_value * std_df / np.sqrt(n)
+
+# print(std_df)
+# print(confidence_interval)
+# formatted_df = pd.Series({col: f"{avg:.2f} ± {std:.2f}" for col, avg, std in zip(avg_df.columns, avg_df.iloc[0], std_df.iloc[0])}).to_frame().T
+formatted_df = pd.Series({col: f"{avg:.2f} ± {ci:.2f}" for col, avg, ci in zip(avg_df.columns, avg_df.iloc[0], confidence_interval.iloc[0])}).to_frame().T
 formatted_df.insert(0,'exp_name', name)
 meta_test_df_human_readable_updated = pd.concat((meta_test_df_human_readable, formatted_df))
 meta_test_df_human_readable_updated.to_csv(OUTPUT_PATH_HR, index=False)
 
+# print(formatted_df)
 avg_df_renamed = avg_df.rename(columns=lambda x: f"{x}_avg")
 std_df_renamed = std_df.rename(columns=lambda x: f"{x}_std")
 
